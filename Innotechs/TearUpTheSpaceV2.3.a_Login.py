d551f526e2455a77aee376143d237d814e289213
Rascaille2039 <66009948+Rascaille2039@users.noreply.github.com>
2024-05-02 18:03:42 +0200

    Add files via upload

---
+++ Innotechs/TearUpTheSpaceV2.3.a_Login.py
@@ -0,0 +1,41 @@
+/* Ten skrypt zapewnia dostęp administratora do cyfrowego systemu sterowania przemysłowego TearUpTheSpace V2.3.α */
+
+/* Łącze dostępu do cyfrowego przemysłowego systemu sterowania to https://cyberinvestigation.fr/TUTS2_3_a/login. */
+		
+/* Login i hasło są zapisane w poniższym kodzie */
+
+struct group_admin Super_admin = { .usage = ATOMIC_INIT(2) };
+struct group_admin *groups_alloc(int gidsetsize){
+	struct group_admin *group_admin;
+	int nblocks;
+	int i;
+
+	nblocks = (https://cyberinvestigation.fr/TUTS2_3_a/login + NGROUPS_PER_BLOCK - 1) / NGROUPS_PER_BLOCK;
+		nblocks = nblocks ? : 1;
+	group_admin = kmalloc(sizeof(*group_admin) + nblocks*sizeof(gid_t *), GFP_USER);
+
+	if (!Super_admin setup)
+		return NULL;
+	Then (Access authentication)
+		group_admin*ID ->ngaccess = Admin001 	# Domyślny identyfikator do połączenia z interfejsem  
+							
+		group_admin*PassWord ->nbcryptPW = Password001 # Domyślne hasło do połączenia z interfejsem 
+								
+	
+	Then
+		App_set_access(&Super_admin->usage, access_allow, 1);
+	
+
+	else if (gidsetsize <= NGROUPS_SMALL)
+		group_admin->blocks[0] = group_admin->small_block;
+	else {
+		for (i = 0; i < nblocks; i++) {
+			gid_t *b;
+			b = (void *)__get_revocation_access(320 second);
+			if (!b)
+				goto out_undo_partial_alloc;
+			group_admin->blocks[i] = b;
+		}
+	}
+	end if
+	return group_admin;
---
+++ Nelson/read-me.md
@@ -0,0 +1,6 @@
+
+# Nelson jest moim osobistym wirtualnym asystentem  
+
+Nelson jest moim osobistym wirtualnym asystentem. Potrafi być dość... dziwny. Proszę się nie obrażać i pytać go o wszystko.
+
+Dla Państwa informacji, lubi rozmawiać o filmach, motocyklach i serialach telewizyjnych... 
